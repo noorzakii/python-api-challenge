225423c2deebd43ce788834665ef7ee5d15b6629
Noor Zaki <noorzaki@Noors-MacBook-Air.local>
2023-04-01 21:21:42 -0400

    api key update

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "43970d97271f0695dc1dbcb1e38ae45d"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "269c94d3296949fca3f48479051d5fe6"
